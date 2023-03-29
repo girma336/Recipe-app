944cff6e1f9964f1446f50fdb63a9d943d1f1f28
girma <girmatarekegn.gi@gmail.com>
2023-03-29 17:14:17 +0300

    Add inventory food controller

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,16 @@ Rails.application.routes.draw do
   root "users#index"
   resources :recipes, only: [:index, :show, :create, :new, :destroy]
   resources :foods, only: [:index, :new, :create, :destroy] 
-  resources :inventories, only: [:index, :show, :create, :new, :destroy]
+  # resources :inventories, only: [:index, :show, :create, :new, :destroy] do
+  #   resources :inventory_foods, only: [:create, :new]
+  # end
+  get '/inventories', to: 'inventories#index'
+  get '/inventories/new', to: 'inventories#new'
+  post '/inventories', to: 'inventories#create'
+  get '/inventories/:id', to: 'inventories#show'
+  delete '/inventories/:id', to: 'inventories#destroy'
+  delete '/inventory_foods/:id', to: 'inventory_foods#destroy'
+  get '/inventories/:id/inventory_foods/new', to: 'inventory_foods#new'
+  post '/inventories/:id/inventory_foods', to: 'inventory_foods#create'
+  # delete '/inventories/:id/inventory_foods/:id', to: 'inventory_foods#create'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_29_051848) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_29_083619) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -80,6 +80,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_29_051848) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "inventories", "users"
+  add_foreign_key "inventory_foods", "foods"
+  add_foreign_key "inventory_foods", "inventories"
   add_foreign_key "recipe_foods", "foods"
   add_foreign_key "recipe_foods", "recipes"
   add_foreign_key "recipes", "users"
